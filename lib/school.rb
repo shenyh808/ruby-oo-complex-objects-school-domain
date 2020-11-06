e27af74d44f99a103273d8b8e2a92d3bd5c703ca
shen huang <shenyh33@gmail.com>
2020-11-05 22:09:19 -1000

    commit tree c41b84bcd5b65605c07b178fb0f84e0319d36bd3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+ 
+     attr_accessor :name, :roster
+ 
+     def add_student(name, number)
+      @roster[number] = [] if roster[number].nil?
+      @roster[number].push(name)
+    end
+ 
+     def grade(number)
+      @roster[number]
+    end
+ 
+     def sort
+      @roster.each do |_key, value|
+        value.sort!
+      end
+      @roster
+    end
+  end
